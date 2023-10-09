8dc426ad23af1484a14f423242ba33f5c7cfd6e4
sajib37 <sajibhasan9811@gmail.com>
2023-10-09 14:47:14 +0600

    testimonial page completed

--- src/Pages/Team/Team.jsx
+++ src/Pages/Team/Team.jsx
@@ -5,10 +5,14 @@ const Team = () => {
 
     const teamMembers = useLoaderData();
     return (
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-            {
-                teamMembers.map((team,idx)=><TeamMember key={idx} team={team}></TeamMember>)
-            }
+        <div className="mt-8 md:mt-12 mb-4">
+            <h1 className='text-3xl md:text-5xl font-bold text-center mb-4 font-serif'>Our Team Members</h1>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 ">
+                
+                {
+                    teamMembers.map((team,idx)=><TeamMember key={idx} team={team}></TeamMember>)
+                }
+            </div>
         </div>
     );
 };
--- src/Shared/Services/Services.jsx
+++ src/Shared/Services/Services.jsx
@@ -23,7 +23,7 @@ const Services = () => {
     }
     return (
         <div className='mt-8 md:mt-12 mb-4'>
-            <h1 className='text-5xl font-bold text-center mb-4'>Our Services</h1>
+            <h1 className='text-3xl md:text-5xl font-bold text-center mb-4'>Our Services</h1>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 px-2 '>
                 {
                     allEvents.slice(0,displayData).map((service,idx)=><Service key={idx} service={service}></Service>)
